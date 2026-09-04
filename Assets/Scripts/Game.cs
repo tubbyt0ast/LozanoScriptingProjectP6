7f7b37c2c62eb7b19b75c2c7fea3754a3bb079ee
tubbyt0ast <731406@student.dusd.net>
2026-09-04 10:48:47 -0700

    creatingprops

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player myPlayer = new Player();
+
+        myPlayer.Experience = 5;
+        int x = myPlayer.Experience;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
